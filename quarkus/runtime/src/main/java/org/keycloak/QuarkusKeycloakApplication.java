d619359dbf956aa7d412d1453320989ae4f23d1e
Thomas Darimont <thomas.darimont@googlemail.com>
2021-09-14 00:51:03 +0200

    KEYCLOAK-XXX PoC for pluggable metrics

--- quarkus/runtime/src/main/java/org/keycloak/QuarkusKeycloakApplication.java
+++ quarkus/runtime/src/main/java/org/keycloak/QuarkusKeycloakApplication.java
@@ -8,8 +8,12 @@ import javax.inject.Inject;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.ApplicationPath;
 
+import io.smallrye.metrics.MetricRegistries;
+import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.keycloak.models.utils.PostMigrationEvent;
 import org.keycloak.provider.quarkus.QuarkusPlatform;
+import org.keycloak.services.metrics.KeycloakMetrics;
+import org.keycloak.services.metrics.KeycloakMetricsCollector;
 import org.keycloak.services.resources.KeycloakApplication;
 import org.keycloak.services.resources.QuarkusWelcomeResource;
 import org.keycloak.services.resources.WelcomeResource;
@@ -29,11 +33,13 @@ public class QuarkusKeycloakApplication extends KeycloakApplication {
         try {
             forceEntityManagerInitialization();
             initializeKeycloakSessionFactory();
+            initializeKeycloakMetrics();
             setupScheduledTasks(sessionFactory);
         } catch (Throwable cause) {
             QuarkusPlatform.exitOnError(cause);
         }
     }
+
 
     @Override
     public Set<Object> getSingletons() {
@@ -51,6 +57,13 @@ public class QuarkusKeycloakApplication extends KeycloakApplication {
         sessionFactory = instance;
         instance.init();
         sessionFactory.publish(new PostMigrationEvent());
+    }
+
+    private void initializeKeycloakMetrics() {
+        MetricRegistry metricsRegistry = MetricRegistries.get(MetricRegistry.Type.APPLICATION);
+        KeycloakMetricsCollector keycloakMetricsCollector = new KeycloakMetricsCollector(sessionFactory, metricsRegistry);
+        KeycloakMetrics keycloakMetrics = new KeycloakMetrics(metricsRegistry, keycloakMetricsCollector);
+        keycloakMetrics.init();
     }
 
     private void forceEntityManagerInitialization() {
