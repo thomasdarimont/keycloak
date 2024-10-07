988c8f888c09e3baf94fffe458890a9cdcee123d
Thomas Darimont <thomas.darimont@googlemail.com>
2024-10-07 22:50:13 +0200

    Next iteration of SSF support
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- services/src/main/java/org/keycloak/protocol/ssf/caep/events/SessionRevoked.java
+++ services/src/main/java/org/keycloak/protocol/ssf/caep/events/SessionRevoked.java
@@ -1,6 +1,6 @@
 package org.keycloak.protocol.ssf.caep.events;
 
-import org.keycloak.protocol.ssf.SecurityEvent;
+import org.keycloak.protocol.ssf.set.SecurityEvent;
 
 /**
  * Session Revoked signals that the session identified by the subject has been revoked. The explicit session identifier may be directly referenced in the subject or other properties of the session may be included to allow the receiver to identify applicable sessions.
