299b3e58d15f8b1864a0bc61cb5b6533913b9ebe
Thomas Darimont <thomas.darimont@googlemail.com>
2024-10-17 10:09:28 +0200

    Next iteration of shared signals framework support
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- services/src/main/java/org/keycloak/protocol/ssf/transmitter/TransmitterMetadata.java
+++ services/src/main/java/org/keycloak/protocol/ssf/transmitter/TransmitterMetadata.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class SsfTransmitterMetadata {
+public class TransmitterMetadata {
 
     @JsonProperty("spec_version")
     private String specVersion;
