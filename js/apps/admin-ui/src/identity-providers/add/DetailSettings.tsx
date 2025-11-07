ae61ec85f7104f44c4249a5915b536d662c1c5eb
Thomas Darimont <thomas.darimont@googlemail.com>
2025-11-07 08:47:05 +0100

    WIP Next iteration of SSF support
    - Represent SSF Receivers as Identity Providers in Admin UI
    - Gradually move from SsfReceiverModel to SsfReceiverProviderConfig
    - Move to external stream management model (streams are created outside of Keycloak)
    - Move verification functionality to SsfReceiverProvider
    - Make SsfReceiverManager obsolete
    
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- js/apps/admin-ui/src/identity-providers/add/DetailSettings.tsx
+++ js/apps/admin-ui/src/identity-providers/add/DetailSettings.tsx
@@ -69,6 +69,7 @@ import { OIDCGeneralSettings } from "./OIDCGeneralSettings";
 import { ReqAuthnConstraints } from "./ReqAuthnConstraintsSettings";
 import { SamlGeneralSettings } from "./SamlGeneralSettings";
 import { SpiffeSettings } from "./SpiffeSettings";
+import { SsfReceiverSettings } from "./SsfReceiverSettings";
 import { AdminEvents } from "../../events/AdminEvents";
 import { UserProfileClaimsSettings } from "./OAuth2UserProfileClaimsSettings";
 import { KubernetesSettings } from "./KubernetesSettings";
@@ -418,6 +419,7 @@ export default function DetailSettings() {
   const isSAML = provider.providerId!.includes("saml");
   const isOAuth2 = provider.providerId!.includes("oauth2");
   const isSPIFFE = provider.providerId!.includes("spiffe");
+  const isSsfReceiver = provider.providerId!.includes("ssf-receiver");
   const isKubernetes = provider.providerId!.includes("kubernetes");
   const isSocial = !isOIDC && !isSAML && !isOAuth2;
   const isJwtAuthorizationGrantSupported =
@@ -453,7 +455,7 @@ export default function DetailSettings() {
   const sections = [
     {
       title: t("generalSettings"),
-      isHidden: isSPIFFE || isKubernetes,
+      isHidden: isSPIFFE || isKubernetes || isSsfReceiver,
       panel: (
         <FormAccess
           role="manage-identity-providers"
@@ -527,6 +529,20 @@ export default function DetailSettings() {
     },
     {
       title: t("generalSettings"),
+      isHidden: !isSsfReceiver,
+      panel: (
+        <Form
+          isHorizontal
+          className="pf-v5-u-py-lg"
+          onSubmit={handleSubmit(save)}
+        >
+          <SsfReceiverSettings />
+          <FixedButtonsGroup name="idp-details" isSubmit reset={reset} />
+        </Form>
+      ),
+    },
+    {
+      title: t("generalSettings"),
       isHidden: !isKubernetes,
       panel: (
         <Form
@@ -559,7 +575,7 @@ export default function DetailSettings() {
     },
     {
       title: t("advancedSettings"),
-      isHidden: isSPIFFE || isKubernetes,
+      isHidden: isSPIFFE || isKubernetes || isSsfReceiver,
       panel: (
         <FormAccess
           role="manage-identity-providers"
@@ -611,7 +627,7 @@ export default function DetailSettings() {
           </Tab>
           <Tab
             id="mappers"
-            isHidden={isSPIFFE || isKubernetes}
+            isHidden={isSPIFFE || isKubernetes || isSsfReceiver}
             data-testid="mappers-tab"
             title={<TabTitleText>{t("mappers")}</TabTitleText>}
             {...mappersTab}
